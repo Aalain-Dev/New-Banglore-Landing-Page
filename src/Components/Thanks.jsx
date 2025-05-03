e64204ccfbdbbc2ac1b23e44fdcea8ee2fb383e0
Aalain-Dev <aalainsmst@gmail.com>
2025-05-03 17:10:14 +0530

    Thanks Text in Center

--- src/Components/Thanks.jsx
+++ src/Components/Thanks.jsx
@@ -10,7 +10,7 @@ const Thanks = () => {
   <div className="mt-30">
 <div className="flex justify-center items-center flex-col">
 <img src={img} alt="" className='xl:w-[400px] w-[300px]' />
-  <h1 className='capitalize font-bold xl:text-3xl text-2xl blue-color mt-10 mb-10'>Our Team will get in Touch with you</h1>
+  <h1 className='capitalize font-bold xl:text-3xl text-2xl blue-color mt-10 mb-10 text-center'>Our Team will get in Touch with you</h1>
 </div>
   <Footer/>
   </div>
