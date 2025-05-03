551893d3011c585dc09db8a4f4eb320d93c2a43f
Aalain-Dev <aalainsmst@gmail.com>
2025-05-03 12:46:56 +0530

    Thanks Page images and content added

--- src/Components/Thanks.jsx
+++ src/Components/Thanks.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-
+import img from "../../src/assets/Thank-you/thanks.webp"
 const Thanks = () => {
   return (
  <>
   <Navbar/>
 
-  <div className="mt-50">
+  <div className="mt-30">
+<div className="flex justify-center items-center flex-col">
+<img src={img} alt="" className='xl:w-[400px] w-[300px]' />
+  <h1 className='capitalize font-bold xl:text-3xl text-2xl blue-color mt-10 mb-10'>Our Team will get in Touch with you</h1>
+</div>
   <Footer/>
   </div>
  </>
