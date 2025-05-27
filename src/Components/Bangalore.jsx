0933d21a169a5d61c2e29ce2b5011f103afccccc
Aalain-Bizdateup-Dev <aalain@bizdateup.com>
2025-05-27 14:08:04 +0530

    Mobile Banner Updated

--- src/Components/Bangalore.jsx
+++ src/Components/Bangalore.jsx
@@ -10,7 +10,7 @@ import { Link as ScrollLink } from 'react-scroll';
 const Bangalore = () => {
   return (
  <>
-    <section className="flex  mt-20 gap-10 flex-col overflow-hidden items-center justify-center xl:pt-20 relative pt-10 pb-20 bg-white text-center px-4" style={{ background: `url(${bangalore_banner})`, backgroundSize: 'cover' }}
+    <section className="flex  mt-20 gap-10 flex-col overflow-hidden items-center justify-center xl:pt-20 relative pt-10 pb-20 bg-white text-center px-4 bg-image-investor" 
 >
      
          {/* <div className="absolute top-0  left-0">
@@ -41,7 +41,39 @@ const Bangalore = () => {
      Request Your Invite
       </ScrollLink>
     </section>
- 
+    <section className="flex  mt-20 gap-10 flex-col overflow-hidden items-center justify-center xl:pt-20 relative pt-10  bg-white text-center px-4 bg-image-investor-mobile " 
+>
+     
+         {/* <div className="absolute top-0  left-0">
+          <img src={bangalore_banner} alt="" className="w-full h-full" />
+        </div> */}
+      <div className="flex flex-col justify-center items-center gap-10 mt-30 mb-60">
+         
+      <h1 className="  text-[1.4rem] font-bold mt-10   text-[#211F52] z-2 ">
+       
+        
+       Capital Connect Bangalore  <br />
+        Exclusive Investor Gathering
+      </h1>
+      <ul class="event-info-list mt-[10px] z-2 font-bold justify-center items-center  gap-3 text-[#211F52] ">
+        <li className="xl:text-[20px] text-[15px] z-2 ">21 June, 2025</li>
+        <li className="xl:text-[20px] text-[15px] z-2 ">10:30 am</li>
+
+        <li className="xl:text-[20px] text-[15px] z-2  ">
+          <a class="underline" target = "_blank" href="https://www.google.com/maps?sca_esv=3470b58f8723632d&rlz=1C1RXMK_enIN997IN997&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiHUlDQiBJTkRJQU4gQ1JBRlQgQlJFV0VSWSBtYXBzMgsQLhiABBjHARivATIIEAAYgAQYogQyCBAAGIAEGKIEMggQABiABBiiBDIIEAAYgAQYogQyGhAuGIAEGMcBGK8BGJcFGNwEGN4EGOAE2AEBSLcJUABY2AhwAHgAkAEAmAGHAqABkQeqAQUwLjEuM7gBA8gBAPgBAvgBAZgCBKACowfCAhAQLhiABBhDGMcBGIoFGK8BwgIFEAAYgATCAh8QLhiABBhDGMcBGIoFGK8BGJcFGNwEGN4EGOAE2AEBmAMA4gMFEgExIEC6BgYIARABGBSSBwUwLjEuM6AH6iGyBwUwLjEuM7gHowc&um=1&ie=UTF-8&fb=1&gl=in&sa=X&geocode=KVfFE5vbF647MRgqfJGU7rWH&daddr=Sy+no+90/3,+K,+572/90,+Outer+Ring+Rd,+beside+Manhpo+Convention+Center,+DadaMastan+Layout,+Manayata+Tech+Park,+Nagawara,+Bengaluru,+Karnataka+560045" target="_blank">
+            ICB INDIAN CRAFT BREWERY, Bangalore
+          </a>
+        </li>
+      </ul>
+      <p className="text-lg   z-2 font-bold   text-[#211F52] ">
+        Discover India's Next Unicorns - By Invitation Only
+      </p>
+<ScrollLink to="tickets" smooth={true} duration={500}    className="z-2  bg-[#211F52] text-white px-6 py-3 cursor-pointer rounded transition-transform duration-300 transform hover:scale-105 xl:w-100 w-60 xl:text-xl"
+>
+     Request Your Invite
+      </ScrollLink>
+      </div>
+    </section>
  </>
   );
 };
--- src/Components/Payment_Thanks.jsx
+++ src/Components/Payment_Thanks.jsx
@@ -10,7 +10,7 @@ const Payment_Thanks = () => {
     <Navbar/>
      
     <div className="mt-30">
-<div className="flex justify-center items-center flex-col">
+<div className="flex justify-center items-center flex-col">   
 <img src={img} alt="" className='xl:w-[400px] w-[300px]' />
   <h1 className='capitalize font-bold xl:text-3xl text-2xl blue-color mt-10 mb-2 text-center'>Our Team will get in Touch with you</h1>
   <h1 className='capitalize font-bold xl:text-2xl text-2xl blue-color mt-2 mb-10 text-center'>Alternatively you can contact <a href="https://wa.me/9987581351" target="_blank" className='underline'>Prasad On Whatsapp</a></h1>
