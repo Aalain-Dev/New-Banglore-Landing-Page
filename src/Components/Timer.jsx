f8c41f2e7f35926217b517b5f4739271481abb3a
Aalain-Bizdateup-Dev <aalain@bizdateup.com>
2025-06-09 09:31:05 +0530

    Increased Timer Date

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-scroll';
 const Timer = () => {
   // Set your countdown target date
-  const targetDate = new Date("2025-06-10T00:00:00").getTime();
+  const targetDate = new Date("2025-06-13T00:00:00").getTime();
 
   const [timeLeft, setTimeLeft] = useState({
     days: "00",
